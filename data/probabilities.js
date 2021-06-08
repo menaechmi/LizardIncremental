75648dd68c678ead69a0cd9cb6d0db52f2739271
menaechmi <menaechmi@protonmail.com>
2021-06-07 22:11:49 -0500

    minor change of "=" to ":" to get it recognized by JS

--- data/probabilities.js
+++ data/probabilities.js
@@ -1,18 +1,18 @@
-Probabilities {
-lizardProbability = {
-    "Anole": 20,
-    "Grass Lizards": 14,
-    "Brown Basilisk": 10,
-    "Rainbow Rock Lizard": 10,
-    "Iguana": 10,
-    "Bearded Dragon": 9,
-    "Skink": 8,
-    "Uromastyx": 7,
-    "Chameleon": 4,
-    "Giant Plated Lizard": 4,
-    "Legless Lizard": 2,
-    "Keeled Lizard": 1,
-    "Frilled Dragons": 1
+let Probabilities = {
+    lizardProbability: {
+        "Anole": 20,
+        "Grass Lizards": 14,
+        "Brown Basilisk": 10,
+        "Rainbow Rock Lizard": 10,
+        "Iguana": 10,
+        "Bearded Dragon": 9,
+        "Skink": 8,
+        "Uromastyx": 7,
+        "Chameleon": 4,
+        "Giant Plated Lizard": 4,
+        "Legless Lizard": 2,
+        "Keeled Lizard": 1,
+        "Frilled Dragons": 1
     }
 }
 export default Probabilities;
