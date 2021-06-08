38cd42758e8233d0a2bfa18806c83e83b0021f35
menaechmi <menaechmi@protonmail.com>
2021-06-07 22:54:32 -0500

    added breed probabilities

--- data/probabilities.js
+++ data/probabilities.js
@@ -13,6 +13,34 @@ let Probabilities = {
         "Legless Lizard": 2,
         "Keeled Lizard": 1,
         "Frilled Dragons": 1
+        "Gecko": 4,
+
+    },
+    breeds: {
+        "Blue Bellied": 15,
+        "Tailless": 25,
+        "Spotted": 35,
+        "Potbellied": 4,
+        "Fire-breathing": 2,
+        "Flying": 7,
+        "Book-eater": 13,
+        "Nocturnal": 5,
+        "Crepuscular": 9,
+        "Cold", 23,
+        "Warm-blooded": 4,
+        "Striped": 35,
+        "Striated": 22,
+        "Two-tailed": 3,
+        "Oklahoma": 25,
+        "Water": 12,
+        "Common": 51,
+        "Panther": 6,
+        "Satanic": 3,
+        "Spiny-tailed": 23,
+        "Leopard": 10,
+        "Fat-tailed": 8,
+        "Argentine": 4,
+        "Argus": 14
     }
 }
 export default Probabilities;
