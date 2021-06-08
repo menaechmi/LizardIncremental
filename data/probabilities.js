cf6aa6d26651effe3b4c2281597b07ad7d73f0be
menaechmi <menaechmi@protonmail.com>
2021-06-08 18:17:17 -0500

    were you aware that arrays needs commas to seperate them? I had forgotten

--- data/probabilities.js
+++ data/probabilities.js
@@ -1,57 +1,58 @@
 let Probabilities = {
     lizardProbability: [
-        ["Anole", 20]
-        ["Grass Lizards", 14]
-        ["Brown Basilisk", 10]
-        ["Rainbow Rock Lizard", 10]
-        ["Iguana", 10]
-        ["Bearded Dragon", 9]
-        ["Skink", 8]
-        ["Uromastyx", 7]
-        ["Chameleon", 4]
-        ["Giant Plated Lizard", 4]
-        ["Legless Lizard", 2]
-        ["Keeled Lizard", 1]
-        ["Frilled Dragons", 1]
+        ["Anole", 20],
+        ["Grass Lizards", 14],
+        ["Brown Basilisk", 10],
+        ["Rainbow Rock Lizard", 10],
+        ["Iguana", 10],
+        ["Bearded Dragon", 9],
+        ["Skink", 8],
+        ["Uromastyx", 7],
+        ["Chameleon", 4],
+        ["Giant Plated Lizard", 4],
+        ["Legless Lizard", 2],
+        ["Keeled Lizard", 1],
+        ["Frilled Dragons", 1],
         ["Gecko", 5]
     ],
     breeds: [
-        ["Blue Bellied", 15]
-        ["Tailless", 25]
-        ["Spotted", 35]
-        ["Potbellied", 4]
-        ["Fire-breathing", 2]
-        ["Flying", 7]
-        ["Book-eater", 13]
-        ["Nocturnal", 5]
-        ["Crepuscular", 9]
-        ["Cold", 23]
-        ["Warm-blooded", 4]
-        ["Striped", 35]
-        ["Striated", 22]
-        ["Two-tailed", 3]
-        ["Oklahoma", 25]
-        ["Water", 12]
-        ["Common", 51]
-        ["Panther", 6]
-        ["Satanic", 3]
-        ["Spiny-tailed", 23]
-        ["Leopard", 10]
-        ["Fat-tailed", 8]
-        ["Argentine", 4]
-        ["Argus", 14]
-        ["Blue Tongue", 12]
-        ["False", 6]
-        ["Giant", 4]
-        ["Neotropical", 8]
-        ["True", 12]
-        ["Citrus Tiger", 6]
-        ["Painted", 4]
-        ["Red Eyed", 21]
-        ["Fat Tailed", 16]
-        ["American", 27]
-        ["House", 17]
+        ["Blue Bellied", 15],
+        ["Tailless", 25],
+        ["Spotted", 35],
+        ["Potbellied", 4],
+        ["Fire-breathing", 2],
+        ["Flying", 7],
+        ["Book-eater", 13],
+        ["Nocturnal", 5],
+        ["Crepuscular", 9],
+        ["Cold", 23],
+        ["Warm-blooded", 4],
+        ["Striped", 35],
+        ["Striated", 22],
+        ["Two-tailed", 3],
+        ["Oklahoma", 25],
+        ["Water", 12],
+        ["Common", 51],
+        ["Panther", 6],
+        ["Satanic", 3],
+        ["Spiny-tailed", 23],
+        ["Leopard", 10],
+        ["Fat-tailed", 8],
+        ["Argentine", 4],
+        ["Argus", 14],
+        ["Blue Tongue", 12],
+        ["False", 6],
+        ["Giant", 4],
+        ["Neotropical", 8],
+        ["True", 12],
+        ["Citrus Tiger", 6],
+        ["Painted", 4],
+        ["Red Eyed", 21],
+        ["Fat Tailed", 16],
+        ["American", 27],
+        ["House", 17],
         ["Crested", 13]
-    ]
+    ],
+
 }
 export default Probabilities;
